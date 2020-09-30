f59d2a032a3bb459c9ce3081263942c197c1a7f2
Patricia <patriciafaustinobr@gmail.com>
2020-09-30 18:51:25 -0300

    Fiz os exercicios sobre comandos de atribuicao

---
+++ aula entrada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aula_entrada {
+    class Program {
+        static void Main(string[] args) {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string s = Console.ReadLine();
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
